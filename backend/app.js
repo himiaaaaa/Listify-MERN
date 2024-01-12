66f43eee03af89e1811e468dfa96949ce085ce85
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-01-12 12:18:05 +0200

    fix payload too large

--- backend/app.js
+++ backend/app.js
@@ -48,7 +48,7 @@ mongoose
 
 app.use(cors())
 app.use(express.static('build'))
-app.use(express.json())
+app.use(express.json({limit: '50mb'}))
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 app.use(middleware.userExtractor)
